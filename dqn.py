038b5a9883d1c40e4c67f1d4b0b4d267b8cf242b
aadila6 <aabudureheman@gmail.com>
2020-03-15 03:15:23 -0700

    Update

--- dqn.py
+++ dqn.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-import math, random
+import math
+import random
 USE_CUDA = torch.cuda.is_available()
-Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+Variable = lambda *args, **kwargs: autograd.Variable(
+    *args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
 
 class QLearner(nn.Module):
     def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
@@ -19,8 +22,8 @@ class QLearner(nn.Module):
         self.env = env
         self.input_shape = self.env.observation_space.shape
         self.num_actions = self.env.action_space.n
-        
-        #defining the graphs with initialzig 
+
+        # defining the graphs with initialzig
         self.features = nn.Sequential(
             nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
             nn.ReLU(),
@@ -29,30 +32,29 @@ class QLearner(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
         )
-        
+
         self.fc = nn.Sequential(
             nn.Linear(self.feature_size(), 512),
             nn.ReLU(),
             nn.Linear(512, self.num_actions)
         )
-        
+
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
-    
+
     def feature_size(self):
-            return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
-    
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+
     def act(self, state, epsilon):
         if random.random() > epsilon:
-            state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
+            state = Variable(torch.FloatTensor(
+                np.float32(state)).unsqueeze(0), requires_grad=True)
             # TODO: Given state, you should write code to get the Q value and chosen action
-            #action = random.randrange(self.env.action_space.n)
             q_value = self.forward(state)
-            action  = q_value.max(1)[1].data[0]
-        
+            action = q_value.max(1)[1].data[0]
         else:
             action = random.randrange(self.env.action_space.n)
         return action
@@ -60,24 +62,32 @@ class QLearner(nn.Module):
     def copy_from(self, target):
         self.load_state_dict(target.state_dict())
 
-        
+
 def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
     state = Variable(torch.FloatTensor(np.float32(state)))
-    next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)))
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
     # implement the loss function here
-    q_values      = model(state)
-    next_q_values = model(next_state)
-    q_value          = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
-    next_q_value     = next_q_values.max(1)[0]
-    expected_q_value = reward + gamma * next_q_value * (1 - done)
-    loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
-    return loss
 
+    # q_values = target_model(state.squeeze(1))
+    # next_q_values = target_model(next_state)
+    # q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    # next_q_value = next_q_values.max(1)[0]
+    # expected_q_value = reward + gamma * next_q_value * (1 - done)
+    # loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
+
+    q_values = model(state)
+    next_q_values = target_model(next_state)
+    q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q_value = next_q_values.max(1)[0]
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
+    loss = (q_value - expected_q_value.detach()).pow(2).mean()
+    
+    return loss
 
 class ReplayBuffer(object):
     def __init__(self, capacity):
@@ -86,16 +96,13 @@ class ReplayBuffer(object):
     def push(self, state, action, reward, next_state, done):
         state = np.expand_dims(state, 0)
         next_state = np.expand_dims(next_state, 0)
-
         self.buffer.append((state, action, reward, next_state, done))
 
     def sample(self, batch_size):
         # TODO: Randomly sampling data with specific batch size from the buffer
         state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
         return np.concatenate(state), action, reward, np.concatenate(next_state), done
-
-
-        # return state, action, reward, next_state, done
-
+        
+    
     def __len__(self):
         return len(self.buffer)
